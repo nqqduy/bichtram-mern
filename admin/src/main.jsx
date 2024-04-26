d390d46092b0d250233f6701ab1c60352fa86bbf
Duy Nguyen <duynguyen1332000@gmail.com>
2024-04-26 16:56:37 +0700

    header bg

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -4,9 +4,29 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import store from "./app/store";
+import { ConfigProvider } from "antd";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token
+          colorPrimary: "#00b96b",
+          borderRadius: 2,
+
+          // Alias Token
+          colorBgContainer: "#f6ffed",
+        },
+        components: {
+          Table: {
+            headerBg: "",
+            headerColor: "",
+          },
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
   </Provider>
 );
